62d694a77ca85c0b08301fae8050c14a6f2b67ac
Timofey Inozemcev <timofey.inozemtsev@evocargo.com>
2022-06-07 20:18:48 +0300

    ljltkfnm!

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -28,7 +28,7 @@ def courses_factory():
 
 # проверка получения 1го курса (retrieve-логика)
 @pytest.mark.django_db
-def test_create_courses(client, courses_factory):
+def test_create_course(client, courses_factory):
     # создаем курс через фабрику
     course = courses_factory()
     count = Course.objects.count()
@@ -39,9 +39,10 @@ def test_create_courses(client, courses_factory):
     # проверяем, что вернулся именно тот курс, который запрашивали
     assert data['name'] == course.name
 
+
 # проверка получения списка курсов (list-логика)
 @pytest.mark.django_db
-def test_get_messages(client, courses_factory):
+def test_get_courses(client, courses_factory):
     # аналогично – сначала вызываем фабрики, затем делаем запрос и проверяем результат
     # Arrange
     courses = courses_factory(_quantity=10)
@@ -49,11 +50,8 @@ def test_get_messages(client, courses_factory):
     response = client.get('/api/v1/courses/')
     # Assert
     data = response.json()
-    from pprint import pprint
-    pprint(data)
     # Все тесты должны явно проверять код возврата.
     assert response.status_code == 200
-    print(len(courses))
     assert len(data) == len(courses)
     for i, m in enumerate(data):
         assert m['name'] == courses[i].name
@@ -61,11 +59,38 @@ def test_get_messages(client, courses_factory):
 
 # проверка фильтрации списка курсов по id
 # создаем курсы через фабрику, передать id одного курса в фильтр, проверить результат запроса с фильтром
+@pytest.mark.django_db
+def test_filter_id(client, courses_factory):
+    courses = courses_factory(_quantity=10)
+    c = 5
+    response = client.get(f'/api/v1/courses/?id={c}')
+    data = response.json()
+    # Все тесты должны явно проверять код возврата.
+    assert response.status_code == 200
+    assert data[0]['name'] == courses[c - 1].name
+
 
 # проверка фильтрации списка курсов по name
+@pytest.mark.django_db
+def test_filter_id(client, courses_factory):
+    course = courses_factory()
+    response = client.get(f'/api/v1/courses/?name={course.name}')
+    data = response.json()
+    # Все тесты должны явно проверять код возврата.
+    assert response.status_code == 200
+    assert data[0]['name'] == course.name
+
 
 # тест успешного создания курса
 # здесь фабрика не нужна, готовим JSON-данные и создаем курс
+@pytest.mark.django_db
+def test_create_course(client):
+    count = Course.objects.count()
+
+    response = client.post('/courses/', data={'user': user.id, 'text': 'test text'})
+
+    assert response.status_code == 201
+    assert Message.objects.count() == count + 1
 
 # тест успешного обновления курса
 # сначала через фабрику создаем, потом обновляем JSON-данными
